011800b5e0c0f1334dcf2d9dffa17bb21374710e
Richard Simbana <richardsimbana@gmail.com>
2016-10-31 10:47:54 -0400

    Adding entire Unity Project

---
+++ 1_Assignment/Assets/Scripts/Collectable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+			Destroy (gameObject);
+	}
+
+}
